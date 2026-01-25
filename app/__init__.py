494a119f88444edcd0b4c948fb92515097996295
Cuneiform Team <team@example.com>
2026-01-24 18:36:45 -0700

    fix: switch to gevent-websocket worker for proper WebSocket support on Render

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from flask_jwt_extended import JWTManager
 
 # Initialize extensions
 db = SQLAlchemy()
-socketio = SocketIO(cors_allowed_origins="*", async_mode='threading')
+socketio = SocketIO(cors_allowed_origins="*", async_mode='gevent')
 jwt = JWTManager()
 
 
